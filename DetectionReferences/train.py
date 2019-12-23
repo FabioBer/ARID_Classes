a14c0b8133f411303ac415451772ad4e4a4b5209
FabioBer <58628606+FabioBer@users.noreply.github.com>
2019-12-23 16:57:55 +0100

    Update train.py

--- DetectionReferences/train.py
+++ DetectionReferences/train.py
@@ -9,13 +9,13 @@ import torchvision
 import torchvision.models.detection
 import torchvision.models.detection.mask_rcnn
 
-from coco_utils import get_coco, get_coco_kp
+from DetectionReferences.coco_utils import get_coco, get_coco_kp
 
-from group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
-from engine import train_one_epoch, evaluate
+from DetectionReferences.group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
+from DetectionReferences.engine import train_one_epoch, evaluate
 
-import utils
-import transforms as T
+import DetectionReferences.utils
+import DetectionReferences.transforms as T
 
 
 def get_dataset(name, image_set, transform, data_path):
